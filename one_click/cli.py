b8c990c3ca226d55193e342be585fb94c6cc7546
Augustus Ostow <ostowster@gmail.com>
2019-01-22 16:14:22 -0800

    Cli script and setuptools to make it executable from the shell

---
+++ one_click/cli.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import click
+import python_terraform as pt 
+
+
+BASE_DIR = Path(__file__).parent
+TERRAFORM_DIR = BASE_DIR / 'terraform'
+
+@click.command()
+@click.option('--public_key_path', default='~/.ssh/id_rsa.pub')
+@click.option('--private_key_path', default='~/.ssh/id_rsa')
+@click.argument('git_path')
+def main(git_path, public_key_path=None, private_key_path=None):
+    var = {
+    'path_to_public_key': public_key_path,
+    'path_to_private_key': private_key_path,
+    'github_clone_link': git_path
+    }
+
+    tf = pt.Terraform(working_dir=TERRAFORM_DIR)
+    tf.apply(var=var, capture_output=False)
+
+
