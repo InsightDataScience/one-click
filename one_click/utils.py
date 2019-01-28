d6ac96981c89d3f2fe5d9e6b6cb8ddad885136ba
Augustus Ostow <ostowster@gmail.com>
2019-01-28 11:13:05 -0800

    Remove trailing slash from local paths

--- one_click/utils.py
+++ one_click/utils.py
@@ -2,6 +2,8 @@ from typing import Dict, Optional
 
 import click
 
+from one_click import cli
+
 
 def dict_to_tfvars(vars: Dict[str, str]) -> str:
     """
@@ -19,6 +21,36 @@ def py_version_to_image(py_version: str) -> Optional[str]:
             param_hint=["3.7", "3.6", "3.5", "2.7"],
         )
     return f"tiangolo/uwsgi-nginx-flask:python{py_version}"
+
+
+def build_and_validate_tfvars(
+    project_link_or_path,
+    public_key_path,
+    private_key_path,
+    py,
+    deployment_source="github",
+):
+    # Drop trailing slash from local path so it doesn't interfere with rsync
+    if (deployment_source == "local") and (project_link_or_path[-1] == "/"):
+        project_link_or_path = project_link_or_path[:-1]
+
+    github_local_switches = {
+        "github": {"use_github": 1, "use_local": 0},
+        "local": {"use_github": 0, "use_local": 1},
+    }
+
+    image_tag = py_version_to_image(py)
+
+    var = {
+        "base_directory": str(cli.BASE_DIR),
+        "path_to_public_key": public_key_path,
+        "path_to_private_key": private_key_path,
+        "project_link_or_path": project_link_or_path,
+        "image_version": image_tag,
+        **github_local_switches[deployment_source],
+    }
+
+    return dict_to_tfvars(var)
 
 
 def pre_destroy_check(deployment_directory):
